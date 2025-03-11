011faed17d440e1583a9caac8ed9feaa49a19dcf
Olivia Chapman <olivia.chapman@capycino-studios.com>
2025-03-11 14:43:59 +0000

    Added icons and nav bar

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
 
 // Services
 import apiService from "@/services/apiService"
@@ -20,6 +22,13 @@ const pinia = createPinia()
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    }
 })
 
 const app = createApp(App);
